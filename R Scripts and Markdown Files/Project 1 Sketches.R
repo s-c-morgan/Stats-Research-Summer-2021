a4eb0213d2ae24084a6c7b19503de5f5589b1959
S.C Morgan <57040387+s-c-morgan@users.noreply.github.com>
2021-05-24 14:01:12 -0400

    Setting up Laplacian

--- R Scripts and Markdown Files/Project 1 Sketches.R
+++ R Scripts and Markdown Files/Project 1 Sketches.R
@@ -5,14 +5,20 @@ library(readr)
 
 
 authors <- read_table2("ca-sandi_auths.csv")
+auth_temp<-authors$node1
+authors$node1<-authors$node2
+authors$node2<-auth_temp
 authlist<- unique(c(authors$node1, authors$node2))
+
+
+
 g.authors<- graph.data.frame(authors[1:2], vertices = authlist, directed = FALSE)
 E(g.authors)$weight<-authors$weight
 plot(g.authors)
 A<-get.adjacency(g.authors)
-
-degree(g.authors)
-weighted_degree<-E(g.authors)$weight*A
+A<-as.matrix(A)
+D<-diag(strength(g.authors))
+U<-D-A
 
 
 
