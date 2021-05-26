98dbc6b74c87fc0c87171f319db028d7af54b49f
S.C Morgan <57040387+s-c-morgan@users.noreply.github.com>
2021-05-26 14:52:41 -0400

    unweighted clustering
    need to implement into markdown file

--- R Scripts and Markdown Files/Project 1 Sketches.R
+++ R Scripts and Markdown Files/Project 1 Sketches.R
@@ -19,6 +19,7 @@ plot(g.authors)
 A<-get.adjacency(g.authors)
 A<-as.matrix(A)
 D<-diag(strength(g.authors))
+D_un<-diag(degree(g.authors))
 
 W<-A
 for (i in 1:nrow(authors)){
@@ -26,57 +27,166 @@ for (i in 1:nrow(authors)){
   W[as.character(authors$node2[i]), as.character(authors$node1[i])]<-authors$weight[i]
 }
 
-L_unnorm<-D-W
+L_unnorm_w<-D-W
+L_unnorm_u<-D_un-A
 
-plot(1:10,rev(evL$values)[1:10], log="y")
-evL <- eigen(L_unnorm, symmetric=TRUE)
+##Weighted unnormalized spectral clustering
+evL_w <- eigen(L_unnorm_w, symmetric=TRUE)
+plot(1:10,rev(evL_w$values)[1:10], log="y")
 
 k   <- 2
-Z   <- evL$vectors[,(ncol(evL$vectors)-k+1):ncol(evL$vectors)]
+Z   <- evL_w$vectors[,(ncol(evL_w$vectors)-k+1):ncol(evL_w$vectors)]
 
+set.seed(123)
 km <- kmeans(Z, centers=k, nstart=5)
+unw2<-km$cluster
 V(g.authors)$cluster<-km$cluster
 V(g.authors)[cluster == 1]$color<-"red"
 V(g.authors)[cluster == 2]$color<-"dodgerblue"
+set.seed(123)
 plot(g.authors)
 
 k   <- 3
-Z   <- evL$vectors[,(ncol(evL$vectors)-k+1):ncol(evL$vectors)]
+Z   <- evL_w$vectors[,(ncol(evL_w$vectors)-k+1):ncol(evL_w$vectors)]
 
+set.seed(123)
 km <- kmeans(Z, centers=k, nstart=5)
+unw3<-km$cluster
 V(g.authors)$cluster<-km$cluster
 V(g.authors)[cluster == 1]$color<-"red"
 V(g.authors)[cluster == 2]$color<-"dodgerblue"
 V(g.authors)[cluster == 3]$color<-"yellow"
+set.seed(123)
+plot(g.authors)
+##Unweighted unnormalized spectral clustering
+evL_u <- eigen(L_unnorm_u, symmetric=TRUE)
+plot(1:10,rev(evL_u$values)[1:10], log="y")
+
+k   <- 2
+Z   <- evL_u$vectors[,(ncol(evL_u$vectors)-k+1):ncol(evL_u$vectors)]
+
+set.seed(123)
+km <- kmeans(Z, centers=k, nstart=5)
+unu2<-km$cluster
+V(g.authors)$cluster<-km$cluster
+V(g.authors)[cluster == 1]$color<-"red"
+V(g.authors)[cluster == 2]$color<-"dodgerblue"
+set.seed(123)
+plot(g.authors)
+
+k   <- 3
+Z   <- evL_u$vectors[,(ncol(evL_u$vectors)-k+1):ncol(evL_u$vectors)]
+
+set.seed(123)
+km <- kmeans(Z, centers=k, nstart=5)
+unu3<-km$cluster
+V(g.authors)$cluster<-km$cluster
+V(g.authors)[cluster == 1]$color<-"red"
+V(g.authors)[cluster == 2]$color<-"dodgerblue"
+V(g.authors)[cluster == 3]$color<-"yellow"
+set.seed(123)
 plot(g.authors)
 
 
 D_neghalf<-1/sqrt(D)
 D_neghalf[D_neghalf==Inf]<-0
-L_normsym<-D_neghalf %*% L_unnorm %*% D_neghalf
+L_sym_w<-D_neghalf %*% L_unnorm_w %*% D_neghalf
 
-evL_sym <- eigen(L_normsym, symmetric=TRUE)
+D_un_neghalf<-1/sqrt(D_un)
+D_un_neghalf[D_un_neghalf==Inf]<-0
+L_sym_u<-D_un_neghalf %*% L_unnorm_u %*% D_un_neghalf
+
+##Weighted normalized symmetric spectral clustering
+evL_sym_w <- eigen(L_sym_w, symmetric=TRUE)
 
 k   <- 2
-Z   <- evL_sym$vectors[,(ncol(evL_sym$vectors)-k+1):ncol(evL_sym$vectors)]
+Z   <- evL_sym_w$vectors[,(ncol(evL_sym_w$vectors)-k+1):ncol(evL_sym_w$vectors)]
+
+set.seed(123)
 km <- kmeans(Z, centers=k, nstart=5)
 V(g.authors)$cluster<-km$cluster
+nsymw2<-km$cluster
 V(g.authors)[cluster == 1]$color<-"red"
 V(g.authors)[cluster == 2]$color<-"dodgerblue"
+set.seed(123)
 plot(g.authors)
 
 k   <- 3
-Z   <- evL_sym$vectors[,(ncol(evL_sym$vectors)-k+1):ncol(evL_sym$vectors)]
+Z   <- evL_sym_w$vectors[,(ncol(evL_sym_w$vectors)-k+1):ncol(evL_sym_w$vectors)]
 
+set.seed(123)
+km <- kmeans(Z, centers=k, nstart=5)
+V(g.authors)$cluster<-km$cluster
+nsymw3<-km$cluster
+V(g.authors)[cluster == 1]$color<-"red"
+V(g.authors)[cluster == 2]$color<-"dodgerblue"
+V(g.authors)[cluster == 3]$color<-"yellow"
+set.seed(123)
+plot(g.authors)
+
+##Unweighted normalized symmetric spectral clustering
+
+evL_sym_u <- eigen(L_sym_u, symmetric=TRUE)
+
+k   <- 2
+Z   <- evL_sym_u$vectors[,(ncol(evL_sym_u$vectors)-k+1):ncol(evL_sym_u$vectors)]
+
+set.seed(123)
+km <- kmeans(Z, centers=k, nstart=5)
+V(g.authors)$cluster<-km$cluster
+nsymu2<-km$cluster
+V(g.authors)[cluster == 1]$color<-"red"
+V(g.authors)[cluster == 2]$color<-"dodgerblue"
+set.seed(123)
+plot(g.authors)
+
+k   <- 3
+Z   <- evL_sym_u$vectors[,(ncol(evL_sym_u$vectors)-k+1):ncol(evL_sym_u$vectors)]
+
+set.seed(123)
+km <- kmeans(Z, centers=k, nstart=5)
+V(g.authors)$cluster<-km$cluster
+nsymu3<-km$cluster
+V(g.authors)[cluster == 1]$color<-"red"
+V(g.authors)[cluster == 2]$color<-"dodgerblue"
+V(g.authors)[cluster == 3]$color<-"yellow"
+set.seed(123)
+plot(g.authors)
+
+
+D_negone_w<-1/D
+D_negone_w[D_negone_w==Inf]<-0
+L_rw_w<-D_negone_w %*% L_unnorm_w
+
+D_negone_u<-1/D_un
+D_negone_u[D_negone_u==Inf]<-0
+L_rw_w<-D_negone_u %*% L_unnorm_u
+
+evL_rw_w <- eigen(L_rw_w, symmetric=FALSE)
+
+k   <- 2
+Z   <- evL_rw_w$vectors[,(ncol(evL_rw_w$vectors)-k+1):ncol(evL_rw_w$vectors)]
+
+set.seed(123)
 km <- kmeans(Z, centers=k, nstart=5)
 V(g.authors)$cluster<-km$cluster
 V(g.authors)[cluster == 1]$color<-"red"
 V(g.authors)[cluster == 2]$color<-"dodgerblue"
-V(g.authors)[cluster == 3]$color<-"yellow"
-setseed(123)
+set.seed(123)
 plot(g.authors)
 
+k   <- 3
+Z   <- evL_rw_w$vectors[,(ncol(evL_rw_w$vectors)-k+1):ncol(evL_rw_w$vectors)]
 
+set.seed(123)
+km <- kmeans(Z, centers=k, nstart=5)
+V(g.authors)$cluster<-km$cluster
+
+V(g.authors)[cluster == 1]$color<-"red"
+V(g.authors)[cluster == 2]$color<-"dodgerblue"
+V(g.authors)[cluster == 3]$color<-"yellow"
+set.seed(123)
+plot(g.authors)
 
 
 
